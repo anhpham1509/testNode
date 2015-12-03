5a395447e445795db58696213277447681adc1f9
Anh Pham <duyanhpham1509@gmail.com>
2015-12-03 21:21:22 +0200

    test mysql

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+
+var index = function (req, res, next) {
+    var collection = {};
+    res.render('index', collection);
+};
+
+var about = function(req, res, next){
+    var collection = {};
+    res.render('index', collection);
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index);
 
-
+/* GET about page. */
+router.get('/about*', about);
 
 module.exports = router;
